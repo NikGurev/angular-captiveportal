9f43acdcb077935692cd063bdf45625d8d43c6d0
NGurev <Nikita.Gurev@lanit-tercom.com>
2020-04-10 16:18:48 +0300

    добавлен роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
 import {AccountComponent} from './components/account/account.component';
+import {PageDesignComponent} from './components/account/page-design/page-design.component';
+import {HomeComponent} from './components/account/home/home.component';
+import {NavigationComponent} from "./components/account/navigation/navigation.component";
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LogInComponent },
-  { path: 'account', component: AccountComponent },
+  { path: 'account', component: AccountComponent, children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'design', component: PageDesignComponent },
+      { path: 'other', component: NavigationComponent }
+    ]},
 ];
 
 @NgModule({
